4ef43246a4a9ff767860a97dfc70631493581451
Brad Martin <bradwaynemartin@gmail.com>
2019-06-17 15:39:58 -0500

    updates demo

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,27 +1,33 @@
 import { ToastDuration, ToastPosition, Toasty } from 'nativescript-toasty';
+import { Color } from 'tns-core-modules/color/color';
 
 export function shortToast() {
   new Toasty('Short Toast').show();
 }
 
 export function longToast() {
-  new Toasty('Long Toast', ToastDuration.LONG)
+  new Toasty('Long Toast with Red Text', ToastDuration.LONG)
     .setToastPosition(ToastPosition.BOTTOM)
+    .setTextColor(new Color('white'))
+    .setBackgroundColor('#ff9999')
     .show();
 }
 
 export function chainedToast() {
-  new Toasty('Chained Toast Methods')
+  const t = new Toasty('Chained Toast Methods')
     .setToastDuration(ToastDuration.LONG)
     .setToastPosition(ToastPosition.CENTER)
-    .show();
+    .setTextColor('blue');
+  t.backgroundColor = 'white';
+  t.show();
 }
 
 export function positionToast() {
   const toast = new Toasty('Position Toast');
-  console.log('setting position');
   toast.position = ToastPosition.TOP;
   toast.duration = ToastDuration.SHORT;
+  toast.textColor = 'black';
+  toast.backgroundColor = new Color('#fff000');
   toast.show();
 }
 
